b516b1f13c5dbe29919f74de696f982f4241d3d7
MAX <ilya.hanin463@gmail.com>
2026-02-10 21:01:24 +0300

    +UI TEST поиск более 5 результатов

--- Practice2/week_2/tests/test_github_careers.py
+++ Practice2/week_2/tests/test_github_careers.py
@@ -11,5 +11,5 @@ def test_careers(page):
     assert "/careers-home" in page.url     # проверяем что произошёл переход на careers сайт
 
     heading = page.locator('h1[token-data="LP.SKU-A1.HEADER"]')  # локатор главного заголовка Careers страницы
-    heading.wait_for(state="visible", timeout=10000)             # ждём пока заголовок реально появится (страница тяжёлая)
+    heading.wait_for(state="visible", timeout=20000)             # ждём пока заголовок реально появится (страница тяжёлая)
     assert heading.is_visible()                                  # финальная проверка: заголовок виден → страница открылась
