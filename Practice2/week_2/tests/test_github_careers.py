4edea9aeff5f3e4763d6c198518e3066dc6597c7
MAX <ilya.hanin463@gmail.com>
2026-03-12 22:42:13 +0300

    правки сиай

--- Practice2/week_2/tests/test_github_careers.py
+++ Practice2/week_2/tests/test_github_careers.py
@@ -2,14 +2,15 @@ from week_2.pages.github_about_page import AboutPage
 
 
 def test_careers(page):
-    about_page = AboutPage(page)  # создаём объект страницы About
-    about_page.open()             # открываем https://github.com/about
+    about_page = AboutPage(page)
+    about_page.open()
 
-    assert about_page.is_opened() is True  # проверяем что About реально загрузилась
+    assert about_page.is_opened() is True
 
-    about_page.click_careers()             # кликаем по карточке Careers
-    assert "/careers-home" in page.url     # проверяем что произошёл переход на careers сайт
+    about_page.click_careers()
 
-    heading = page.locator('h1[token-data="LP.SKU-A1.HEADER"]')  # локатор главного заголовка Careers страницы
-    heading.wait_for(state="visible", timeout=20000)             # ждём пока заголовок реально появится (страница тяжёлая)
-    assert heading.is_visible()                                  # финальная проверка: заголовок виден → страница открылась
+    heading = page.locator('h1[token-data="LP.SKU-A1.HEADER"]')
+    heading.wait_for(state="visible", timeout=20000)
+
+    assert "github.careers" in page.url
+    assert heading.is_visible()
