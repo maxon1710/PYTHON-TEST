f720a247f7161901bb91ee1f7e742086f8b46ad8
MAX <ilya.hanin463@gmail.com>
2026-03-13 15:48:12 +0300

    правки сиай

--- Practice2/fixtures/ui_fixtures.py
+++ Practice2/fixtures/ui_fixtures.py
@@ -1,6 +1,7 @@
+# ui_fixtures.py
 import pytest
+import allure
 from playwright.sync_api import sync_playwright
-
 from config.settings import HEADLESS
 
 
@@ -14,4 +15,19 @@ def page(request):
         page = browser.new_page()
         page.set_viewport_size({"width": 1920, "height": 1080})
         yield page
+
+        # После теста — проверяем упал ли и делаем скриншот
+        rep = getattr(request.node, "rep_call", None)
+        if rep and rep.failed:
+            allure.attach(
+                page.screenshot(),
+                name="screenshot_on_failure",
+                attachment_type=allure.attachment_type.PNG
+            )
+            allure.attach(
+                page.content(),
+                name="page_html",
+                attachment_type=allure.attachment_type.HTML
+            )
+
         browser.close()
