e18ecb10e42746e38e0317b1c0962e4ee4686475
MAX <ilya.hanin463@gmail.com>
2026-03-13 15:28:24 +0300

    правки сиай

--- Practice2/conftest.py
+++ Practice2/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-
+import allure
 
 pytest_plugins = (
     "fixtures.ui_fixtures",
@@ -15,3 +15,41 @@ def pytest_addoption(parser):
         default=False,
         help="Run Playwright in headless mode.",
     )
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, f"rep_{rep.when}", rep)
+
+
+@pytest.fixture(autouse=True)
+def attach_on_failure(request):
+    yield
+
+    rep = getattr(request.node, "rep_call", None)
+    if not rep or not rep.failed:
+        return
+
+    page = request.node.funcargs.get("page")
+
+    if page is not None:
+        # UI тест
+        allure.attach(
+            page.screenshot(),
+            name="screenshot_on_failure",
+            attachment_type=allure.attachment_type.PNG
+        )
+        allure.attach(
+            page.content(),
+            name="page_html",
+            attachment_type=allure.attachment_type.HTML
+        )
+    else:
+        # API тест
+        allure.attach(
+            str(rep.longrepr),
+            name="failure_details",
+            attachment_type=allure.attachment_type.TEXT
+        )
