540408aba0fb51758a2dac21c7f110329c324926
MAX <ilya.hanin463@gmail.com>
2026-03-21 17:44:32 +0300

    правки сиай

--- Practice2/conftest.py
+++ Practice2/conftest.py
@@ -35,19 +35,20 @@ def attach_on_failure(request):
     page = request.node.funcargs.get("page")
 
     if page is not None:
-        # UI тест
-        allure.attach(
-            page.screenshot(),
-            name="screenshot_on_failure",
-            attachment_type=allure.attachment_type.PNG
-        )
-        allure.attach(
-            page.content(),
-            name="page_html",
-            attachment_type=allure.attachment_type.HTML
-        )
+        try:
+            allure.attach(
+                page.screenshot(),
+                name="screenshot_on_failure",
+                attachment_type=allure.attachment_type.PNG
+            )
+            allure.attach(
+                page.content(),
+                name="page_html",
+                attachment_type=allure.attachment_type.HTML
+            )
+        except Exception:
+            pass
     else:
-        # API тест
         allure.attach(
             str(rep.longrepr),
             name="failure_details",
